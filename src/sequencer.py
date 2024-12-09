6b4920ba7579df3f53cbbe6c5a441c42e5d05240
ralf <ralfoide@gmail.com>
2024-12-09 12:03:49 -0800

    Sequencer: Parser.

--- src/sequencer.py
+++ src/sequencer.py
@@ -1,10 +1,118 @@
 #
 
-class Sequencer():
+import re
+import time
+
+class Rgb():
+    def __init__(self, rgb: str):
+        if len(rgb) != 6 and len(rgb) != 7:
+            raise ValueError("Sequencer: RGB expected in hex format [#?]RRGGBB but was '%s'" % rgb)
+        if rgb.startswith("#"):
+            rgb = rgb[1:]
+        self.r = int(rgb[0:2], 16)
+        self.g = int(rgb[2:4], 16)
+        self.b = int(rgb[4:6], 16)
+
+    def __eq__(self, rhs):
+        if not isinstance(rhs, self.__class__):
+            return False
+        return (self.r == rhs.r
+            and self.g == rhs.g
+            and self.b == rhs.b)
+
+    def __repr__(self):
+        return f"#{self.r:02x}{self.g:02x}{self.b:02x}"
+
+class RgbCount():
+    def __init__(self, rgb: str, count: int):
+        self.rgb = Rgb(rgb)
+        self.count = count
+
+    def __eq__(self, rhs):
+        if not isinstance(rhs, self.__class__):
+            return False
+        return (self.rgb == rhs.rgb
+            and self.count == rhs.count)
+    
+    def __repr__(self):
+        return f"<{self.rgb},{self.count}>"
+
+class Instruction():
     def __init__(self):
         pass
 
-    def foo(self):
-        return 42
+class InstructionSlide(Instruction):
+    def __init__(self, delay: float, count: int):
+        self._delay = delay
+        self._count = count
+    
+    def __eq__(self, rhs):
+        if not isinstance(rhs, self.__class__):
+            return False
+        return (self._count == rhs._count 
+            and self._delay == rhs._delay)
+
+    def __repr__(self):
+        return f"Slide {self._delay} s x {self._count}"
+
+class InstructionFill(Instruction):
+    def __init__(self, delay: float, runs: list[RgbCount]):
+        self._delay = delay
+        self._runs = runs
+    
+    def __eq__(self, rhs):
+        if not isinstance(rhs, self.__class__):
+            return False
+        runs_ok = all(x == y for x, y in zip(self._runs, rhs._runs))
+        if not runs_ok:
+            raise ValueError("runs not ok: %s vs %s" % (self._runs, rhs._runs) )
+        return (runs_ok 
+            and self._delay == rhs._delay)
+    
+    def __repr__(self):
+        return f"Fill {self._delay} s x {self._runs}"
+
+class Sequencer():
+    def __init__(self,
+                sleep_f = time.sleep):
+        # Function overrides for unit tests
+        self._sleep_f = sleep_f
+        self._instructions = []
+        self._pc = 0
+
+    def parse(self, instructions: str) -> None:
+        self._instructions = []
+        self._pc = 0
+        for line in instructions.split(";"):
+            lexems = re.split(r"\s+", line.strip())
+            if not lexems or lexems[0].startswith("#"):
+                # Skip empty line or comment.
+                continue
+            verb = lexems[0].lower()
+            if verb == "fill":
+                if (len(lexems) - 1) % 2 != 0:
+                    raise ValueError("Sequencer: Expected 'Fill <rgb count> pairs' in line '%s'" % line)
+                lexems.pop(0) # skip verb
+                runs = [RgbCount(lexems[i*2], lexems[i*2+1]) for i in range(0, len(lexems) // 2)]
+                inst = InstructionFill(0, runs)
+                self._instructions.append(inst)
+            elif verb == "slowfill":
+                if (len(lexems)) % 2 != 0:
+                    raise ValueError("Sequencer: Expected 'SlowFill delay  <rgb count> pairs' in line '%s'" % line)
+                lexems.pop(0) # skip verb
+                delay = float(lexems.pop(0))
+                runs = [RgbCount(lexems[i*2], lexems[i*2+1]) for i in range(0, len(lexems) // 2)]
+                inst = InstructionFill(delay, runs)
+                self._instructions.append(inst)
+            elif verb == "slide":
+                if len(lexems) < 3:
+                    raise ValueError("Sequencer: Expected 'Slide delay count' in line '%s'" % line)
+                inst = InstructionSlide(float(lexems[1]), int(lexems[2]))
+                self._instructions.append(inst)
+            else:
+                raise ValueError("Sequencer: Unknown command '%s' in line '%s'" % (verb, line))
+    
+    def step(self) -> None:
+        pass
 
 #~~
--- src/test_sequencer.py
+++ src/test_sequencer.py
@@ -1,17 +1,61 @@
 # Test for Sequencer
 
+
 import unittest
+from sequencer import *
 
-from sequencer import Sequencer
 
-class TestAdd(unittest.TestCase):
-    def test_foo(self):
+class TestRgb(unittest.TestCase):
+    def test_rgb1(self):
+        c = Rgb("#112233")
+        self.assertEqual(c.r, 0x11)
+        self.assertEqual(c.g, 0x22)
+        self.assertEqual(c.b, 0x33)
+        self.assertEqual(repr(c), "#112233")
+
+    def test_rgb2(self):
+        c = Rgb("223344")
+        self.assertEqual(c.r, 0x22)
+        self.assertEqual(c.g, 0x33)
+        self.assertEqual(c.b, 0x44)
+        self.assertEqual(repr(c), "#223344")
+
+    def test_eq(self):
+        self.assertEqual(Rgb("#112233"), Rgb("#112233"))
+        self.assertEqual(Rgb("#112233"), Rgb("112233"))
+        self.assertEqual(Rgb("112233"), Rgb("112233"))
+
+
+class TestRgbCount(unittest.TestCase):
+    def test_rgb_count(self):
+        c = RgbCount("#112233", 42)
+        self.assertEqual(c.rgb.r, 0x11)
+        self.assertEqual(c.rgb.g, 0x22)
+        self.assertEqual(c.rgb.b, 0x33)
+        self.assertEqual(c.count, 42)
+        self.assertEqual(repr(c), "<#112233,42>")
+
+    def test_eq(self):
+        self.assertEqual(RgbCount("#112233", 42), RgbCount("#112233", 42))
+        self.assertEqual(RgbCount("#112233", 42), RgbCount("112233", 42))
+
+
+class TestSequencer(unittest.TestCase):
+    def test_seq1(self):
         s = Sequencer()
-        self.assertEqual(s.foo(), 5)
+        s.parse("Fill #FF0000 2  #00FF00 3  #0000FF 4 ; Slide 0.5 42 ; SlowFill 0.42 #FFEEDD 10  #FF0000 11  #00FF00 12  #0000FF 13")
+        self.assertEqual(s._instructions, [
+            InstructionFill(delay=0, runs=[RgbCount("FF0000", 2), RgbCount("00FF00", 3), RgbCount("0000FF", 4)]),
+            InstructionSlide(delay=0.5, count=42),
+            InstructionFill(delay=0.42, runs=[RgbCount("FFEEDD", 10), RgbCount("FF0000", 11), RgbCount("00FF00", 12), RgbCount("0000FF", 13)]),
+        ])
+
 
 if __name__ == '__main__':
     unittest.main()
 
+
 #~~
+
 
 
